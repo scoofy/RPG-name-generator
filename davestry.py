76e62a681f143ad5e84ddc61f64f4bf595c3c91f
davesilb <34032804+davesilb@users.noreply.github.com>
2017-12-05 09:16:21 -0800

    Update davestry.py

--- davestry.py
+++ davestry.py
@@ -19,19 +19,19 @@ def grabNames():
     for nameGroup in fileNames: 
         nameSplit = re.split('([A-Z].*)\.',nameGroup) #break apart entity name(orc, human, tavern, etc.) from element type (L1, etc.)
         with open(filePath + nameGroup) as myfile:
-            fileText = myfile.read()
+            fileText = myfile.read() #read the content of the current file
             
             while True:
-                
-                try:
-                    
+                #store the list of name elements from the current file under the appropriate dictionary key
+                #nameSplit[0] will be the entity type (orc, tavern, etc.)
+                #nameSplit[1] will be the element type (L1, F1M, A1, etc.)
+                try:                    
                     nameDict[nameSplit[0]][nameSplit[1]] = fileText[8:-3].split()
                     break
                 except:
                     nameDict[nameSplit[0]]= {}
 
+    #print (nameDict)
+      
 
-    print (nameDict)
-       
-
-grabNames()
+#grabNames()
