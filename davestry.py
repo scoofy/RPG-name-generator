1bb6c7aa4979ac3d6919f99f282368653b940dac
davesilb <34032804+davesilb@users.noreply.github.com>
2017-12-05 08:54:00 -0800

    Update davestry.py

--- davestry.py
+++ davestry.py
@@ -1,17 +1,24 @@
 import os, re
 
 def grabNames():
+    #load all name data from the names folder into a single dictionary of dictionaries
+    #top level = race/entity (tavern, orc, human, party, etc)
+    #second level =name element (L1, F1F, A1 etc
+    #assumptions:
+    #.    All files in the names fokder contain name data
+    #.    File names are in the format [entity][ELEMENT CODE]
+    #.    File name case is important
+    
     nameDict={}
 
     #filePath = 'C:/Users/David/AppData/Local/Programs/Python/Python36/RPG-name-generator-master/names/'
     filePath = 'names/'
-    fileNames = os.listdir(filePath) #get a list of all the name datafiles
+    fileNames = os.listdir(filePath) #get a list of all the files in the names directory
     fileNames.remove('__init__.py')
-
 
     for nameGroup in fileNames: 
         nameSplit = re.split('([A-Z].*)\.',nameGroup) #Identify the name family (orc, human, tavern, etc.)
-        with open(filePath + nameGroup) as myfile:
+        with open(filePath + nameGroup) as myfile#:
             fileText = myfile.read()
             
             while True:
